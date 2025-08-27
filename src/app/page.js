a814b479e4a40aae81d0b33a67c51ed6a2b6408a
DeepMehta06 <deepmehta2006@gmail.com>
2025-08-27 22:21:18 +0530

    Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,82 @@
 import Image from "next/image";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import { Layers2, Layers3, NotebookPen, Pencil, TypeOutline, ZapIcon } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Stratus",
+  description: "Manage your content with ease",
+};
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="w-full">
+      <section className="flex flex-col items-center-safe justify-center-safe w-full h-[50vh] sm:h-[70vh] md:h-[80vh] lg:h-[80vh] gap-5">
+        <div className="flex flex-col items-center-safe justify-center-safe w-full gap-2">
+          <h1 className={`${poppins.className} text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl mx-1`}>From Ideas to Impact—Fast.</h1>
+          <p className={`${poppins.className} mx-1 text-center`}>Seamless workflows, smarter publishing, and infinite scalability.</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-row gap-5 justify-center items-center">
+          <Link href="/blogs">
+            <Button variant={"outline"} className={"hover:bg-gray-500 transition-all duration-200"}>Try it out</Button>
+          </Link>
+          <Button>Learn More</Button>
+        </div>
+      </section>
+      <section className="min-h-[50vh] sm:min-h-[50vh] bg-gray-600/10 flex justify-center items-center px-4 py-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-8 mx-3">
+          <div className="flex flex-col items-center text-center h-full">
+            <NotebookPen size={50} className="text-blue-400 mb-3" />
+            <h3 className={`${poppins.className} font-semibold text-gray-100 text-xl`}>
+              Intuitive Editor
+            </h3>
+            <p className={`${poppins.className} text-gray-400 w-[80%]`}>
+              Create and edit content effortlessly with a clean, intuitive interface designed for focus and creativity.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center h-full">
+            <Layers3 size={50} className="text-green-400 mb-3" />
+            <h3 className={`${poppins.className} font-semibold text-gray-100 text-xl`}>
+              Flexible Workflows
+            </h3>
+            <p className={`${poppins.className} text-gray-400 w-[80%]`}>
+              Organize, structure, and collaborate seamlessly across teams and projects.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center h-full">
+            <ZapIcon size={50} className="text-yellow-400 mb-3" />
+            <h3 className={`${poppins.className} font-semibold text-gray-100 text-xl`}>
+              Lightning Fast Publishing
+            </h3>
+            <p className={`${poppins.className} text-gray-400 w-[80%]`}>
+              Publish your content instantly across channels with just one click.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="min-h-[50vh] sm:min-h-[50vh] flex justify-center items-center py-4">
+        <div className={`flex flex-col justify-center-safe items-center-safe ${poppins.className} gap-3`}>
+        <h4 className="text-2xl font-bold text-center">Ready to Transform Your Content Writing Journey ?</h4>
+        <p className="text-xl font-medium text-center">Join Thousands of content writers on Stratus</p>
+        <div className="flex gap-3 justify-center items-center">
+        <input className="bg-zinc-700 px-3 py-2 text-center rounded-xl focus:outline-1 focus:outline-gray-400" placeholder="Enter Your Email"></input>
+        <Button className={"rounded-xl"}>Join Now</Button>
+        </div>
+        </div>
+      </section>
+    </main>
   );
 }
