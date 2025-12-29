f208076f714efa59796fbd60ed474729f67bb4ea
DeepMehta06 <deepmehta2006@gmail.com>
2025-12-29 14:54:20 +0530

    Added CORS Policy

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,15 @@ import { NextResponse } from "next/server";
 import { rateLimit } from "./utils/rateLimits";
 
 export async function middleware(request){
+
+    const allowedOrigins = ['https://localhost:3000', 'https://stratuscms.netlify.app/']
     if(request.method === 'POST'){
+        
+        const origin = request.headers.get('origin');
+        if(allowedOrigins.includes(origin)){
+            return NextResponse.json({message : "CUnauthorized domain"}, {status : 403});
+        }
+
         let ip = request.ip || request.headers.get("x-forwarded-for") || 'unknown';
         const {limit, remaining, reset} =await rateLimit.limit(ip);
         if(remaining==0) return NextResponse.json({message : "You have reached the request limit"}, {status : 491})
